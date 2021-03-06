f3e9ee1c336fdc6d2c78d651296a3f38c8bccac9
Karl <karl@localhost.localdomain>
2021-03-06 16:14:31 +0000

    add config map and config secret values

--- src/main/java/com/apitest/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/apitest/helloworld/controller/HelloWorldController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
 	private String text;
+	private String configMap;
+	private String configSecret;
 	
 	public String getText() {
 		return text;
@@ -21,11 +23,28 @@ public class HelloWorldController {
 
 	public void setText(String text) {
 		this.text = text;
+	} 
+	
+	public String getConfigMap() {
+		return configMap;
+	}
+
+	public void setConfigMap(String configMap) {
+		this.configMap = configMap;
+	} 
+	
+	public String getConfigSecret() {
+		return configSecret;
+	}
+
+	public void setConfigSecret(String configSecret) {
+		this.configSecret = configSecret;
 	}  
 
 	@RequestMapping(value = "/helloworld", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String HelloWorld() {  
-	  
-		return getText();  	  
+		
+
+		return "{\"Text\": " + getText() + ",\"Config Map\": \"" + getConfigMap() + "\",\"ConfigSecret\": \"" + getConfigSecret() + "\"}";  	  
 	}	
 }

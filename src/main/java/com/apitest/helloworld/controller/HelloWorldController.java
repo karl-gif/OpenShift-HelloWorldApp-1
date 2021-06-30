1478058a517d427b56b61b86903fbfed101dbe08
karl18 <karl18@scsxnet.uk>
2021-06-30 19:20:41 +0100

    update controller

--- src/main/java/com/apitest/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/apitest/helloworld/controller/HelloWorldController.java
@@ -1,15 +1,11 @@
 package com.apitest.helloworld.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.context.properties.*;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/v1")

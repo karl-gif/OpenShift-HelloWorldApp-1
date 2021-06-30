03bc2d186668f432f0c9e6caf47324ce97400946
karl18 <karl18@scsxnet.uk>
2021-06-30 17:19:54 +0100

    Update app

--- src/main/java/com/apitest/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/apitest/helloworld/controller/HelloWorldController.java
@@ -15,49 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/v1")
 @ConfigurationProperties(prefix="config")
 @Component
-@Slf4j
-public class HelloWorldController {
-
-	private String text;
-	private String configMap;
-	private String configSecret;
-
-	private static final Logger LOGGER = LogManager.getLogger();
-	
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	} 
-	
-	public String getConfigMap() {
-		return configMap;
-	}
-
-	public void setConfigMap(String configMap) {
-		this.configMap = configMap;
-	} 
-	
-	public String getConfigSecret() {
-		return configSecret;
-	}
-
-	public void setConfigSecret(String configSecret) {
-		this.configSecret = configSecret;
-	}  
+public class HelloWorldController {	
 
 	@RequestMapping(value = "/helloworld", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String HelloWorld() {  
-		
-		LOGGER.info("Info logged data");
 
-		return "{\"Text\": " + getText() + ",\"Config Map\": \"" + getConfigMap() + "\",\"ConfigSecret\": \"" + getConfigSecret() + "\"}";  	  
+		return "Spring API 1 - Hello World";  	  
 	}	
-
-
-	
-
 
 }
